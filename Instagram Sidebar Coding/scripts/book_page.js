7f787655d15664fc7d2ecdac4495ec55182aa3f1
smj700f <samsungduos7001@gmail.com>
2023-05-01 12:35:30 +0800

    Change Store and Book_Detail and Book_Page import

--- Instagram Sidebar Coding/scripts/book_page.js
+++ Instagram Sidebar Coding/scripts/book_page.js
@@ -1,12 +1,12 @@
 // 取得按鈕元素和浮動頁面元素
-var btn_open_book_page = document.getElementById("btn-open_book_page");
-var book_page = document.getElementById("book_page");
-var btn_close_book_page = document.getElementById("btn-close_book_page");
+var btn_reading_local_book = document.getElementById("btn_reading_local_book");
+var page_local_book = document.getElementById("page_local_book");
+var btn_close_local_book = document.getElementById("btn_close_local_book");
 // 當按鈕被點擊時，顯示浮動頁面
-btn_open_book_page.addEventListener("click", function() {
-    book_page.style.display = "block";
+btn_reading_local_book.addEventListener("click", function() {
+    page_local_book.style.display = "block";
 });
 // 當關閉按鈕被點擊時，隱藏浮動頁面
-btn_close_book_page.addEventListener("click", function() {
-    book_page.style.display = "none";
+btn_close_local_book.addEventListener("click", function() {
+    page_local_book.style.display = "none";
 });
--- Instagram Sidebar Coding/scripts/store.js
+++ Instagram Sidebar Coding/scripts/store.js
@@ -10,7 +10,7 @@ window.onload = function () {
     let index = 0;
 
     function updateCarousel() {
-        carouselInner.style.transform = `translateY(-${height * index}px)`;
+        carouselInner.style.transform = `translateY(-${height * index * 1.265}px)`;
     }
 
     function nextSlide() {
@@ -31,7 +31,7 @@ window.onload = function () {
 
     function setupCarousel() {
         updateCarousel();
-        setInterval(nextSlide, 6000);
+        setInterval(nextSlide, 3000);
     }
 
     setupCarousel();
